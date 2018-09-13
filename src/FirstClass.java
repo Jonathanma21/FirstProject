d701cf814d6725a81d10bacf5eb15139558f00e2
Jonathanma21 <42975675+Jonathanma21@users.noreply.github.com>
2018-09-13 12:26:12 -0400

    Add files via upload

---
+++ src/FirstClass.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Jonathan Ma
+ * First Program
+ *
+ */
+
+public class FirstClass 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hewwo World!");
+		
+	}
+
+}
